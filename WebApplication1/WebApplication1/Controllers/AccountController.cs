fe472816f6e6c3159cc4b671e9df3b027a25f412
judith kitieu Dongmo <kitjode16@gmail.com>
2016-05-06 12:21:19 +0200

    ajout commentaire

--- WebApplication1/WebApplication1/Controllers/AccountController.cs
+++ WebApplication1/WebApplication1/Controllers/AccountController.cs
@@ -20,6 +20,7 @@ namespace WebApplication1.Controllers
 
         public AccountController()
         {
+            // parfait
         }
 
         public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager )
